9c4773ee1ef6130bb3b6dbea7a97aae4116b7b34
Sheikh MD Sazidul Islam <87424122+Sazidul008@users.noreply.github.com>
2022-05-01 09:03:12 +0600

    simple route and simple navbar set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Inventory from './Pages/Inventory/Inventory';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/inventory' element={<Inventory/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">Sazid's PC Shop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                    </Nav>
+                    <Nav className="d-flex">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/inventory">Inventory</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
